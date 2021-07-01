61c87529f37aa861931a59b79ac62f0b1bf3aa20
Leonardo Teixeira <tteixeira.leonardo@gmail.com>
2021-07-01 12:03:19 -0300

    Imprime as mensagens "Quente" ou "Frio" dentro do <body>

--- script.js
+++ script.js
@@ -14,29 +14,52 @@ hideCat()
 
 window.addEventListener('click', (e) => {
     const click = e.target;
-    const cat = document.getElementById('cat')
+    const cat = document.getElementById('cat');
+    let state = '';
 
     const clickX = e.x;
     const catX = cat.style.transform;
-    const catXNumber = parseInt(catX.slice(10,13))
+    const catXNumber = parseInt(catX.slice(10,13));
 
     const clickY = e.y;
     const catY = cat.style.transform;
-    const catYNumber = parseInt(catY.slice(17,20))
+    const catYNumber = parseInt(catY.slice(17,20));
 
+    if (click == cat) {
+        cat.style.opacity = '1';
+        return;
+    }
     
     if (clickX - catXNumber < 175 
         && clickX - catXNumber > -100 
         && clickY - catYNumber < 175 
         && clickY - catYNumber > -100) {
-        console.log("Quente!")
+        state = 'quente';
     } else {
-        console.log("Frio!")
+        state = 'frio';
     }
 
-    if (click == cat) {
-        cat.style.opacity = '1'
-        console.log(click)
-    }
-})
+    printMessage(e.x, e.y, state);
+});
 
+
+// Imprime as mensagens 'Quente' ou 'Frio' no <body>
+function printMessage(x, y, state) {
+    const message = document.createElement('span');
+    
+    if (state === 'quente') {
+        message.textContent = 'Quente';
+        message.style.transform = `translate(${x - 110}px, ${y - 10}px)`;
+    } else {
+        message.textContent = 'Frio';
+        message.style.transform = `translate(${x - 84}px, ${y - 10}px)`;
+    }
+
+    message.style.position = 'absolute';
+    document.body.appendChild(message);
+
+    // Remove o elemento <span> após 300ms
+    setTimeout(() => {
+        message.remove();
+    }, 300);
+}
