57e72ef46967bba79b06eb25424279f090f19ae1
Gabriela Costa Sertori <64798344+gabrielasertori@users.noreply.github.com>
2021-06-30 23:20:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,16 +2,13 @@
 
 function hideCat () {
     const cat = document.getElementById('cat');
-    
     const x = parseInt(Math.random()*1000);
-    const y = parseInt(Math.random()*1000);
-    console.log(x)
-    console.log(y)
+    const y = parseInt(Math.random()*600);
+    /* console.log(x)
+    console.log(y) */
 
     cat.style.transform = `translate(${x}px, ${y}px)`
-    
 }
-
 hideCat()
 
 
@@ -19,8 +16,27 @@ window.addEventListener('click', (e) => {
     const click = e.target;
     const cat = document.getElementById('cat')
 
+    const clickX = e.x;
+    const catX = cat.style.transform;
+    const catXNumber = parseInt(catX.slice(10,13))
+
+    const clickY = e.y;
+    const catY = cat.style.transform;
+    const catYNumber = parseInt(catY.slice(17,20))
+
+    
+    if (clickX - catXNumber < 175 
+        && clickX - catXNumber > -100 
+        && clickY - catYNumber < 175 
+        && clickY - catYNumber > -100) {
+        console.log("Quente!")
+    } else {
+        console.log("Frio!")
+    }
+
     if (click == cat) {
         cat.style.opacity = '1'
         console.log(click)
     }
 })
+
