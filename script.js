16473daa2b5755ff9922e548ca99a2c7e64c4702
gabriela <gabriela@sertori.com.br>
2021-09-12 00:13:49 -0300

    refactoring

--- script.js
+++ script.js
@@ -6,66 +6,60 @@ function hideCat () {
 	const y = parseInt(Math.random()*600);
 	cat.style.transform = `translate(${x}px, ${y}px)`
 }
-hideCat()
-
+hideCat();
 
 window.addEventListener('click', (e) => {
 	const click = e.target;
 	const cat = document.getElementById('cat');
 	let state = '';
-
-	const clickX = e.x;
-	const catX = cat.style.transform;
-	const catXNumber = parseInt(catX.slice(10,13));
-
-	const clickY = e.y;
-	const catY = cat.style.transform;
-	const catYNumber = parseInt(catY.slice(17,20));
+	let locale = catLocale(e, cat);
 
 	if (click == cat) {
 		cat.style.opacity = '1';
 		return;
 	}
-
-	if (clickX - catXNumber < 175
-		&& clickX - catXNumber > -100
-		&& clickY - catYNumber < 175
-		&& clickY - catYNumber > -100) {
-		state = 'hot'
-
-	} else if (clickX - catXNumber < 275
-		&& clickX - catXNumber > -200
-		&& clickY - catYNumber < 275
-		&& clickY - catYNumber > -200){
-		state = 'warm'
-
+	if (locale[0] < 175 && locale[0] > -100
+		&& locale[1] < 175 && locale[1] > -100) {
+		state = 'hot';
+	} else if (locale[0] < 275 && locale[0] > -200
+		&& locale[1] < 275 && locale[1] > -200){
+		state = 'warm';
 	} else {
-		state = 'cold'
+		state = 'cold';
 	}
 	printMessage(e.x, e.y, state);
 });
 
+// Return the diff between click and cat location
+function catLocale(event, cat) {
+	const clickX = event.x;
+	const catX = cat.style.transform;
+	const catXNumber = parseInt(catX.slice(10,13));
 
-// Print 'warm' or 'cold' on screen
+	const clickY = event.y;
+	const catY = cat.style.transform;
+	const catYNumber = parseInt(catY.slice(17,20));
+
+	return [(clickX - catXNumber), (clickY - catYNumber)];
+}
+
+// Print 'hot', 'warm' or 'cold' on screen
 function printMessage(x, y, state) {
 	const message = document.createElement('span');
 
 	if (state === 'hot') {
 		message.textContent = 'hot';
-		message.style.transform = `translate(${x - 110}px, ${y - 30}px)`;
 	} else if (state == 'warm') {
 		message.textContent = 'warm';
-		message.style.transform = `translate(${x - 84}px, ${y - 30}px)`;
 	} else {
 		message.textContent = 'cold';
-		message.style.transform = `translate(${x - 84}px, ${y - 30}px)`;
 	}
-
+	message.style.transform = `translate(${x - 84}px, ${y - 30}px)`;
 	message.style.position = 'absolute';
 	document.body.appendChild(message);
 	setTimeout(() => {
 		message.remove();
-	}, 300);
+	}, 400);
 }
 
 function start() {
